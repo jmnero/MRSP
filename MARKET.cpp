4996539813e4e38b773d8e3171cd5650c165e0eb
Marvs <samiraaubrey.d.manzano@isu.edu.ph>
2022-12-02 11:50:17 +0800

    Update MARKET.cpp

--- MARKET.cpp
+++ MARKET.cpp
@@ -1,7 +1,43 @@
-#include<iostream>
-using namespace std
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
 
-int main()
-{
-  cout<<"Hello world "; 
+const int COUNT = 17;
+string list[] = {
+  "Apple Juice",
+  "Bubblegum",
+  "Cat Food",
+  "Dundun",
+  "Eggs",
+  "Fudgebar",
+  "San Miguel Gin",
+  "Hansel",
+  "Jackfruit",
+  "Kill this love",
+  "Lemon Juice",
+  "Mango Pie",
+  "Nova",
+  "Oishi",
+  "Piattos",
+  "Queen",
+  "Ror",
+};
+
+void listofproducts(){
+  int num;
+
+  cout <<"LIST OF PRODUCTS"<<endl;
+  for (int a = 0; a < COUNT; a++) {
+    cout <<(a+1)<<" "<<list[a]<<endl;
+  }
+
+  cin>>num;
+
+}
+
+
+
+int main() {
+   listofproducts();
+    return 0;
 }
